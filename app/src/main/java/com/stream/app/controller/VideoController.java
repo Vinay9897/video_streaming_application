390f83f1640378a60edb9b53fcab2e9e998d593d
Vinay9898 <vinayyadav91190@gmail.com>
2024-09-09 19:29:01 +0530

    Revert " design ui page to upload files and reset files"
    This reverts commit 6ebd44beaa1a9cdb2c2c29e9a17e775398c31dc4.

--- app/src/main/java/com/stream/app/controller/VideoController.java
+++ app/src/main/java/com/stream/app/controller/VideoController.java
@@ -5,8 +5,6 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,9 +15,8 @@ import com.stream.app.entities.Video;
 import com.stream.app.payload.CustomMessage;
 import com.stream.app.repositories.VideoRepository;
 import com.stream.app.service.VideoInterface;
-@RequestMapping(value = "/app")
+@RequestMapping("/app")
 @RestController
-@CrossOrigin("*")
 public class VideoController {
 	
 	@Autowired
@@ -28,7 +25,7 @@ public class VideoController {
 	@Autowired
 	VideoRepository videoRepository;
 	
-	@PostMapping("/videos")
+	@RequestMapping("/videos")
 	public ResponseEntity<CustomMessage> create(@RequestParam("file") MultipartFile file, @RequestParam("title") String title, @RequestParam("description") String description) {
 		Video video = new Video();
 		video.setVideoTitle(title);
@@ -36,9 +33,7 @@ public class VideoController {
 		video.setVideoId(UUID.randomUUID().toString());
 		video.setContentType(file.getContentType());
 		video.setFilePath(file.getOriginalFilename());
-		Video response = videoInterface.save(video, file);
-		Video savedVideo = videoRepository.save(response);
-		System.out.println("hi video transfered");
+		Video savedVideo = videoRepository.save(video);
 		if(savedVideo != null)
 		{
 			return ResponseEntity
